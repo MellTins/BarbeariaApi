2e99e30834dab2e933edfb3c64015b6c14689251
Melissa Martins <melmartins.pro@gmail.com>
2026-03-26 15:40:50 -0300

    Endpoints Get criados, um bara osbervar a lista de clientes e outro para procurar clientes pelo id.

---
+++ Controllers/ClienteModelControllers1.cs
@@ -0,0 +1,39 @@
+﻿using BarbeariaApi.Data;
+using BarbeariaApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarbeariaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteModelControllers1 : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ClienteModelControllers1(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<ClienteModel>> BuscarClientes()
+        {
+            var clientes = _context.Clientes.ToList();
+            return Ok(clientes);
+        }
+
+        [HttpGet("id")]
+        public ActionResult<ClienteModel>BuscarClientesPorId(int id)
+        {
+            var cliente = _context.Clientes.Find(id);
+
+            if(cliente == null)
+            {
+                return NotFound("Registro não localizado");
+            }
+
+            return Ok(cliente);
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using BarbeariaApi.Data;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +20,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.MapScalarApiReference();
     app.MapOpenApi();
 }
 
